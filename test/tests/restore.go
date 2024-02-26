57d796383a1e5a8d3abaa31d4dde7215028a23f3
nxcc <>
2024-02-26 10:36:44 +0100

    workaround buggy test

--- test/tests/restore.go
+++ test/tests/restore.go
@@ -60,7 +60,9 @@ func TaskBuildRestoreTests(baseConfig tasks.KV) []goyek.Task {
 		)
 	}
 
-	files := []int{200, 332, 447}
+
+	//files := []int{200, 332, 447}
+	files := []int{447, 447, 447} // workaround buggy test (1.22/loopvar)
 	backupRoot := env["BACKUP_ROOT"]
 
 	// backups = append(backups,
